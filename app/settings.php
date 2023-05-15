fdf19437c2756725ecef1fabec5c431b12f93dd7
火子 <yxyong@126.com>
2023-05-15 10:34:20 +0800

    Update settings.php

--- app/settings.php
+++ app/settings.php
@@ -13,30 +13,19 @@ return function (ContainerBuilder $containerBuilder) {
         'displayErrorDetails' => true, // 显示错误详细信息，在生产中应设置为false
         'logger' => [
           'name' => 'slim-app',
-          'path' => isset($_ENV['docker']) ? 'php://stdout' : realpath('../') . '/var/logs/app.log',
+          'path' => isset($_ENV['docker']) ? 'php://stdout' : ROOT_PATH . '/var/logs/app.log',
           'level' => Logger::DEBUG
         ],
-        'uploadFilePath' => realpath('../var/uploadfiles'),
+        'uploadFilePath' => ROOT_PATH . '/var/uploadfiles',
         'oauth2Config' => [
           // openssl genrsa -aes128 -passout pass:wanphp@1122 -out private.key 2048
           // openssl rsa -in private.key -passin pass:wanphp@1122 -pubout -out public.key
-          //'privateKey' => realpath('../var/conf/key/private.key'),
+          //'privateKey' => ROOT_PATH . '/var/conf/key/private.key',
           // 资源服务器使用
-          'publicKey' => realpath('../var/conf/key/public.key'),
+          'publicKey' => ROOT_PATH . '/var/conf/key/public.key',
           'privateKeyPass' => 'wanphp@1122',
           // echo Key::createNewRandomKey()->saveToAsciiSafeString();
-          'encryptionKey' => 'def000008bd8e66117fe24fd2dacc6c3b777598bbe740dae5581f74fe9363d09c36ae8beaa12b5ef16d091a46fb5ef6c914cf94c2fbac04a2615556a34e7c9f98ed2c397',
-          'storage' => [
-            'type' => 'redis', // redis/mysql
-            'database' => [
-              'parameters' => ['scheme' => 'tcp',
-                'host' => 'redis',
-                'password' => 'wanphp#1122',
-                'port' => 6379,
-                'database' => 2],
-              'options' => ['prefix' => 'uc:']
-            ]
-          ]
+          'encryptionKey' => 'def000008bd8e66117fe24fd2dacc6c3b777598bbe740dae5581f74fe9363d09c36ae8beaa12b5ef16d091a46fb5ef6c914cf94c2fbac04a2615556a34e7c9f98ed2c397'
         ],
         'userServer' => [
           'appId' => 'wanphp',
@@ -65,14 +54,9 @@ return function (ContainerBuilder $containerBuilder) {
           ],
           'error' => PDO::ERRMODE_SILENT
         ],
-        'redis' => [
-          'parameters' => ['scheme' => 'tcp',
-            'host' => 'redis',
-            'password' => 'wanphp#1122',
-            'port' => 6379,
-            'database' => 1],
-          'options' => ['prefix' => 'wp:']
-        ],
+        'AuthCodeStorage' => new \App\Repositories\RedisCacheRepository(
+          new Predis\Client(['scheme' => 'tcp', 'host' => 'redis', 'password' => 'wanphp#1122', 'port' => 6379, 'database' => 2], ['prefix' => 'uc:'])
+        ),
         'wechat.base' => [
           'appid' => '',
           'appsecret' => '',
