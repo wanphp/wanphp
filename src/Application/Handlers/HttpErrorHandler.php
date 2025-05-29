e62f365fefb6ef98d0767558ccaefd4bff781b28
火子 <wanphp@outlook.com>
2025-05-29 10:16:36 +0800

    调整出错日志记录方式

--- src/Application/Handlers/HttpErrorHandler.php
+++ src/Application/Handlers/HttpErrorHandler.php
@@ -4,14 +4,17 @@ declare(strict_types=1);
 namespace App\Application\Handlers;
 
 use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Exception\HttpException;
 use Slim\Handlers\ErrorHandler as SlimErrorHandler;
 
 class HttpErrorHandler extends SlimErrorHandler
 {
   protected function respond(): Response
   {
-    $response = $this->responseFactory->createResponse($this->exception->getCode());
-    $json = json_encode(['code' => $this->exception->getCode(), 'errMsg' => $this->exception->getMessage()], JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
+    $statusCode = 500;
+    if ($this->exception instanceof HttpException) $statusCode = $this->exception->getCode();
+    $response = $this->responseFactory->createResponse($statusCode);
+    $json = json_encode(['code' => $statusCode, 'errMsg' => $this->exception->getMessage()], JSON_PRETTY_PRINT + JSON_UNESCAPED_UNICODE);
     $response->getBody()->write($json);
 
     return $response->withHeader('Content-Type', 'application/json');
